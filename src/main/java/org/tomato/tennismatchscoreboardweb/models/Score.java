5fd231c5b296be5f839089465d08a63c3ab02f10
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-24 10:17:23 +0300

    added util model

---
+++ src/main/java/org/tomato/tennismatchscoreboardweb/models/Score.java
@@ -0,0 +1,12 @@
+package org.tomato.tennismatchscoreboardweb.models;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class Score {
+    private int firstScore;
+    private int secondScore;
+
+}
