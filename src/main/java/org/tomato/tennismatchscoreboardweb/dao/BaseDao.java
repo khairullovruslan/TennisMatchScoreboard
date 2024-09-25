4f1297c9f7e772699ddb97ba90eaea9bc889adaf
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 13:37:37 +0300

    change method's parameters

--- src/main/java/org/tomato/tennismatchscoreboardweb/dao/BaseDao.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/dao/BaseDao.java
@@ -3,6 +3,6 @@ package org.tomato.tennismatchscoreboardweb.dao;
 import org.hibernate.Session;
 
 public interface BaseDao<T> {
-    T findById(long id, Session session);
-    T save(T entity, Session session);
+    T findById(long id);
+    T save(T entity);
 }
