e3ba747fbfa7657aabd462f847cbc19457eb63f9
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 14:13:26 +0300

    delete never used methods

--- src/main/java/org/tomato/tennismatchscoreboardweb/dao/PlayerDao.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/dao/PlayerDao.java
@@ -1,37 +1,24 @@
 package org.tomato.tennismatchscoreboardweb.dao;
 
-import org.hibernate.Session;
 import org.tomato.tennismatchscoreboardweb.models.Player;
 import org.tomato.tennismatchscoreboardweb.util.HibernateUtil;
 
-import java.sql.SQLException;
-import java.util.List;
-
 public class PlayerDao implements BaseDao<Player> {
     private final static PlayerDao INSTANCE = new PlayerDao();
-    private PlayerDao(){
 
+    private PlayerDao() {
     }
 
     public static PlayerDao getInstance() {
         return INSTANCE;
     }
 
-    @Override
-    public Player findById(long id) {
-        var sessionFactory = HibernateUtil.getSessionFactory();
-        try(var session = sessionFactory.openSession();) {
-            return session.get(Player.class, id);
-        }
-
-
-    }
 
     @Override
     public Player save(Player entity) {
 
         var sessionFactory = HibernateUtil.getSessionFactory();
-        try(var session = sessionFactory.openSession();) {
+        try (var session = sessionFactory.openSession()) {
             var transaction = session.beginTransaction();
             session.persist(entity);
             transaction.commit();
@@ -39,27 +26,15 @@ public class PlayerDao implements BaseDao<Player> {
         }
     }
 
-    public Player findByName(String name){
+    public Player findByName(String name) {
         var sessionFactory = HibernateUtil.getSessionFactory();
-        try(var session = sessionFactory.openSession();) {
+        try (var session = sessionFactory.openSession()) {
             String hql = "select p from Player p where p.name = :name";
-            return (Player) session.createQuery(hql)
+            return session.createQuery(hql, Player.class)
                     .setParameter("name", name)
                     .uniqueResult();
         }
 
     }
-    public List<Player> findAll(){
-        var sessionFactory = HibernateUtil.getSessionFactory();
-        try(var session = sessionFactory.openSession();) {
-            var transaction = session.beginTransaction();
-            List<Player> players = session.createQuery("select p from Player p ", Player.class).list();
-            transaction.commit();
-
-            return players;
-        }
-
-    }
-
 
 }
