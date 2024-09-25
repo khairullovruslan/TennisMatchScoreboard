e84367ef75e0984c954cce54bd5b1463ea0b2cb3
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 14:14:47 +0300

    improving code readability by splitting a method into several

--- src/main/java/org/tomato/tennismatchscoreboardweb/services/CalculateTheMatchScoreService.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/services/CalculateTheMatchScoreService.java
@@ -7,7 +7,8 @@ import org.tomato.tennismatchscoreboardweb.models.Score;
 
 public class CalculateTheMatchScoreService {
     private final static CalculateTheMatchScoreService INSTANCE = new CalculateTheMatchScoreService();
-    private CalculateTheMatchScoreService(){
+
+    private CalculateTheMatchScoreService() {
     }
 
     public static CalculateTheMatchScoreService getInstance() {
@@ -15,74 +16,76 @@ public class CalculateTheMatchScoreService {
     }
 
     private final MatchDao matchDao = MatchDao.getInstance();
-    public boolean calculate(MatchScore matchScore, String winner){
+
+    public boolean calculate(MatchScore matchScore, String winner) {
         Score score1 = matchScore.getScore1();
         Score score2 = matchScore.getScore2();
-        if (winner.equals("player1")){
+        if (winner.equals("player1")) {
             settingPoint(matchScore, score1, score2);
-        }
-        else {
+        } else {
             settingPoint(matchScore, score2, score1);
         }
 
         if (score1.getCountSets() == 2 || score2.getCountSets() == 2) {
             Match match = matchScore.getMatch();
             match.setWinner(score1.getCountSets() == 2 ? match.getPlayer1() : match.getPlayer2());
-            try {
-                matchDao.save(match);
-            }
-            catch (Exception e){
-                e.printStackTrace();
-            }
-
+            matchDao.save(match);
             return true;
         }
         return false;
 
     }
-    private void clearGames(Score score1, Score score2){
+
+    private void clearGames(Score score1, Score score2) {
         score1.setCountGames(0);
         score2.setCountGames(0);
     }
-    private void clearPoints(Score score1, Score score2){
+
+    private void clearPoints(Score score1, Score score2) {
         score1.setPoints(0);
         score2.setPoints(0);
     }
-    private void settingPoint(MatchScore matchScore, Score score1, Score score2){
-        if (matchScore.isDop()){
-            score1.setPoints(score1.getPoints() + 1);
-            if (score1.getPoints() >= 7 && score1.getPoints() - score2.getPoints() >= 2){
-                if (score1.getCountGames() == 6 && score2.getCountGames() == 6){
+
+    private void scoringDuringExtraGames(Score score1, Score score2, MatchScore matchScore) {
+        score1.setPoints(score1.getPoints() + 1);
+        if (score1.getPoints() >= 7 && score1.getPoints() - score2.getPoints() >= 2) {
+            if (score1.getCountGames() == 6 && score2.getCountGames() == 6) {
+                score1.setCountSets(score1.getCountSets() + 1);
+                clearGames(score1, score2);
+            } else {
+                score1.setCountGames(score2.getCountGames() + 1);
+            }
+            matchScore.setDop(false);
+            clearPoints(score1, score2);
+        }
+    }
+
+    private void regularTimeScoring(Score score1, Score score2, MatchScore matchScore) {
+        if (score1.getPoints() == 40 && score2.getPoints() == 40) {
+            matchScore.setDop(true);
+
+            score1.setPoints(1);
+            score2.setPoints(0);
+        } else {
+            score1.setPoints(score1.getPoints() + (score1.getPoints() == 30 ? 10 : 15));
+            if (score1.getPoints() > 40) {
+                score1.setCountGames(score1.getCountGames() + 1);
+                if (score1.getCountGames() == 6 && score2.getCountGames() == 6) {
+                    matchScore.setDop(true);
+                } else if (score1.getCountGames() > 6) {
                     score1.setCountSets(score1.getCountSets() + 1);
                     clearGames(score1, score2);
                 }
-                else {
-                    score1.setCountGames(score2.getCountGames() + 1);
-                }
-                matchScore.setDop(false);
                 clearPoints(score1, score2);
             }
         }
-        else {
-            if (score1.getPoints() == 40 && score2.getPoints() == 40){
-                matchScore.setDop(true);
+    }
 
-                score1.setPoints(1);
-                score2.setPoints(0);
-            }
-            else{
-                score1.setPoints(score1.getPoints() + (score1.getPoints() == 30 ? 10 : 15));
-                if (score1.getPoints() > 40){
-                    score1.setCountGames(score1.getCountGames() + 1);
-                    if (score1.getCountGames() == 6 && score2.getCountGames() == 6){
-                        matchScore.setDop(true);
-                    } else if (score1.getCountGames() > 6) {
-                        score1.setCountSets(score1.getCountSets() + 1);
-                        clearGames(score1, score2);
-                    }
-                    clearPoints(score1, score2);
-                }
-            }
+    private void settingPoint(MatchScore matchScore, Score score1, Score score2) {
+        if (matchScore.isDop()) {
+            scoringDuringExtraGames(score1, score2, matchScore);
+        } else {
+            regularTimeScoring(score1, score2, matchScore);
         }
     }
 
