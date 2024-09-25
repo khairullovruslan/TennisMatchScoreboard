44c4898a4301e0fa678e71b47d99a320aff0d9ce
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 13:53:55 +0300

    validate params

--- src/main/java/org/tomato/tennismatchscoreboardweb/controller/MatchScoreServlet.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/controller/MatchScoreServlet.java
@@ -7,70 +7,71 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.tomato.tennismatchscoreboardweb.models.MatchScore;
 import org.tomato.tennismatchscoreboardweb.services.CalculateTheMatchScoreService;
-import org.tomato.tennismatchscoreboardweb.services.PlayerService;
+import org.tomato.tennismatchscoreboardweb.util.RequestParametersValidator;
+
 
 import java.io.IOException;
 import java.util.UUID;
 
 @WebServlet("/match-score")
 public class MatchScoreServlet extends HttpServlet {
-    private final CalculateTheMatchScoreService calculateTheMatchScoreService =  CalculateTheMatchScoreService.getInstance();
+    private final CalculateTheMatchScoreService calculateTheMatchScoreService = CalculateTheMatchScoreService.getInstance();
+    private final RequestParametersValidator validator = RequestParametersValidator.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String param = req.getParameter("uuid");
-        if (param == null){
+        if (param == null) {
             resp.sendError(400, "Нет нужных параметров");
             return;
         }
-        try {
-            UUID uuid = UUID.fromString(param);
-            MatchScore matchScore = MatchScore.getMatch(uuid);
-            if (matchScore == null){
-                resp.sendError(404, "Матч не был найден");
-                return;
-            }
-            req.setAttribute("matchId", String.valueOf(uuid));
-            req.setAttribute("player1", matchScore.getMatch().getPlayer1().getName());
-            req.setAttribute("player2", matchScore.getMatch().getPlayer2().getName());
-            req.setAttribute("score1", matchScore.getScore1());
-            req.setAttribute("score2", matchScore.getScore2());
-
-            req.getRequestDispatcher("WEB-INF/match-score.jsp").forward(req, resp);
-
-        } catch (IllegalArgumentException e) {
-            resp.sendError(400, "Некорректный формат uuid");
+        UUID uuid;
+        if (validator.canConvertToUUID(param)) {
+            uuid = UUID.fromString(param);
+        } else {
+            resp.sendError(400, "Некорректные параметры запроса");
+            return;
         }
-        super.doGet(req, resp);
+
+        MatchScore matchScore = MatchScore.getMatch(uuid);
+        if (matchScore == null) {
+            resp.sendError(404, "Матч не был найден");
+            return;
+        }
+        req.setAttribute("matchId", String.valueOf(uuid));
+        req.setAttribute("player1", matchScore.getMatch().getPlayer1().getName());
+        req.setAttribute("player2", matchScore.getMatch().getPlayer2().getName());
+        req.setAttribute("score1", matchScore.getScore1());
+        req.setAttribute("score2", matchScore.getScore2());
+        req.getRequestDispatcher("WEB-INF/match-score.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String param = req.getParameter("uuid");
         String winner = req.getParameter("winner");
-        if (param == null || winner == null){
+        if (param == null || winner == null) {
             resp.sendError(400, "Отсуствуют необходимые параметры");
             return;
         }
-        try {
-            UUID uuid = UUID.fromString(param);
-            MatchScore matchScore = MatchScore.getMatch(uuid);
-            boolean isEnd = calculateTheMatchScoreService.calculate(matchScore, winner);
-            if (isEnd){
-                req.setAttribute("winner", matchScore.getMatch().getWinner().getName());
-                req.setAttribute("count1", matchScore.getScore1().getCountSets());
-                req.setAttribute("count2", matchScore.getScore2().getCountSets());
-                MatchScore.removeMatch(uuid);
-                req.getRequestDispatcher("WEB-INF/match-end.jsp").forward(req, resp);
-            }
-            else{
-                resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + uuid);
-            }
 
+        UUID uuid;
+        if (validator.canConvertToUUID(param)) {
+            uuid = UUID.fromString(param);
+        } else {
+            resp.sendError(400, "Некорректные параметры запроса");
+            return;
         }
-        catch (IllegalArgumentException e){
-            resp.sendError(400, "Некорректный формат uuid");
+        MatchScore matchScore = MatchScore.getMatch(uuid);
+        boolean isEnd = calculateTheMatchScoreService.calculate(matchScore, winner);
+        if (isEnd) {
+            req.setAttribute("winner", matchScore.getMatch().getWinner().getName());
+            req.setAttribute("count1", matchScore.getScore1().getCountSets());
+            req.setAttribute("count2", matchScore.getScore2().getCountSets());
+            MatchScore.removeMatch(uuid);
+            req.getRequestDispatcher("WEB-INF/match-end.jsp").forward(req, resp);
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + uuid);
         }
-
     }
 }
--- src/main/java/org/tomato/tennismatchscoreboardweb/controller/MatchesServlet.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/controller/MatchesServlet.java
@@ -7,31 +7,40 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.tomato.tennismatchscoreboardweb.models.Match;
 import org.tomato.tennismatchscoreboardweb.services.MatchesService;
+import org.tomato.tennismatchscoreboardweb.util.RequestParametersValidator;
 
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet("/matches")
-public class MatchesServlet  extends HttpServlet {
-    private final MatchesService matchesService =  MatchesService.getInstance();
-    private final int PAGE_SIZE = 10;
+public class MatchesServlet extends HttpServlet {
+    private final MatchesService matchesService = MatchesService.getInstance();
+    private final RequestParametersValidator validator = RequestParametersValidator.getInstance();
+    private final static int PAGE_SIZE = 10;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String pageParam = req.getParameter("page");
         String nameParam = req.getParameter("filter_by_player_name");
         int page = 1;
-        if (pageParam != null) page = Integer.valueOf(pageParam);
-        List<Match> matchList;
-        if (nameParam == null){
-            matchList = matchesService.findAll(page, PAGE_SIZE);
+
+        if (pageParam != null) {
+            if (validator.canConvertToInteger(pageParam)) {
+                page = Integer.parseInt(pageParam);
+            } else {
+                resp.sendError(400, "Некорректные параметры запроса");
+            }
         }
-        else {
+
+        List<Match> matchList;
+        if (nameParam == null) {
+            matchList = matchesService.findAll(page, PAGE_SIZE);
+        } else {
             matchList = matchesService.findAllWithName(page, PAGE_SIZE, nameParam);
         }
 
         req.setAttribute("matches", matchList);
         req.getRequestDispatcher("WEB-INF/all-matches.jsp").forward(req, resp);
-
 
     }
 }
