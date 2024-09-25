e0da527840c31f04703cd9c3c73ef2845560cfd4
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 13:38:33 +0300

    change method's parameters

--- src/main/java/org/tomato/tennismatchscoreboardweb/dao/PlayerDao.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/dao/PlayerDao.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.tomato.tennismatchscoreboardweb.models.Player;
 import org.tomato.tennismatchscoreboardweb.util.HibernateUtil;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class PlayerDao implements BaseDao<Player> {
@@ -17,22 +18,36 @@ public class PlayerDao implements BaseDao<Player> {
     }
 
     @Override
-    public Player findById(long id, Session session) {
-        return session.get(Player.class, id);
+    public Player findById(long id) {
+        var sessionFactory = HibernateUtil.getSessionFactory();
+        try(var session = sessionFactory.openSession();) {
+            return session.get(Player.class, id);
+        }
+
 
     }
 
     @Override
-    public Player save(Player entity, Session session) {
-        session.persist(entity);
-        return entity;
+    public Player save(Player entity) {
+
+        var sessionFactory = HibernateUtil.getSessionFactory();
+        try(var session = sessionFactory.openSession();) {
+            var transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+            return entity;
+        }
     }
 
-    public Player findByName(String name, Session session){
-        String hql = "select p from Player p where p.name = :name";
-        return (Player) session.createQuery(hql)
-                .setParameter("name", name)
-                .uniqueResult();
+    public Player findByName(String name){
+        var sessionFactory = HibernateUtil.getSessionFactory();
+        try(var session = sessionFactory.openSession();) {
+            String hql = "select p from Player p where p.name = :name";
+            return (Player) session.createQuery(hql)
+                    .setParameter("name", name)
+                    .uniqueResult();
+        }
+
     }
     public List<Player> findAll(){
         var sessionFactory = HibernateUtil.getSessionFactory();
