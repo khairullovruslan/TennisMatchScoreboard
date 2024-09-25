f1bb7bb6ee36d4a200860ad8c8f59c3fd75186e5
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-25 12:25:01 +0300

    change HashMap to ConcurrentHashMap and get methods for use map

--- src/main/java/org/tomato/tennismatchscoreboardweb/models/MatchScore.java
+++ src/main/java/org/tomato/tennismatchscoreboardweb/models/MatchScore.java
@@ -1,19 +1,32 @@
 package org.tomato.tennismatchscoreboardweb.models;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.ToString;
+import lombok.*;
 
-import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Builder
 @Data
 @ToString
 public class MatchScore {
-    public static HashMap<UUID, MatchScore> scores;
+    private static ConcurrentHashMap<UUID, MatchScore> scores;
     private Match match;
     private Score score1;
     private Score score2;
     private boolean isDop;
+
+    public static void addMatch(UUID uuid, MatchScore matchScore){
+        if (scores == null){
+            scores = new ConcurrentHashMap<>();
+        }
+        scores.put(uuid, matchScore);
+    }
+    public static MatchScore getMatch(UUID uuid){
+        return scores.getOrDefault(uuid, null);
+    }
+    public static void removeMatch(UUID uuid){
+        scores.remove(uuid);
+    }
+
+
 }
